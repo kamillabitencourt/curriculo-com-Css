8f10272cd85e7003d198a18b30eba3c68db06711
Kamilla <kamillabitencourt18@gmail.com>
2025-02-23 15:41:31 -0300

    primeira versao

---
+++ script.js
@@ -0,0 +1,11 @@
+const backToTop = document.getElementById("backToTop");//botao voltar ao topo
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+backToTop.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
